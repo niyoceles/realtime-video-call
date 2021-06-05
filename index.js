820f37e60a64bd9233584c33ab8450fcc4438801
Celestin Niyonsaba <niyoceles3@gmail.com>
2021-06-05 10:17:26 +0200

    fix server

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 
 //Starts the server
 
-let server = app.listen(3030, function () {
-  console.log("Server is running");
+let server = app.listen(3030,'localhost', function () {
+  console.log("Server running... port %d in %s mode", server.address().port, server.address().address);
 });
 
 app.use(express.static("public"));
